682ca4d21348f7f74f55ec68087c8c3f81e95dda
Omar Jbara <omar.Jbara2@gmail.com>
2018-07-20 02:15:12 +0200

    - Cleaned project - Added basic stuff and project architexture

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
+import "./assets/scss/app.scss";
 import router from "./router";
 import { createProvider } from "./vue-apollo";
 
--- src/router.js
+++ src/router.js
@@ -1,8 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
-import About from "./views/About.vue";
-import TestGraphql from "./views/TestGraphql";
 
 Vue.use(Router);
 
@@ -13,16 +11,6 @@ export default new Router({
       path: "/",
       name: "home",
       component: Home
-    },
-    {
-      path: "/about",
-      name: "about",
-      component: About
-    },
-    {
-        path: "/test-graphql",
-        name: "test-gql",
-        component: TestGraphql
     }
   ]
 });
