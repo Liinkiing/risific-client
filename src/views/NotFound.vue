8e7eb1b6a6b233e1adc9233c52ef2ad35836a504
Omar Jbara <omar.Jbara2@gmail.com>
2018-08-05 00:05:51 +0200

    - Fixed 404 page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,8 +1,14 @@
 <template>
-    <div class="not-found-page">
+    <main class="container not-found-page">
       <vue-headful title="Risific - Page non trouvée"/>
       <h1>404 - Not found</h1>
-    </div>
+      <div class="row centered">
+        <img class="responsive-image" src="https://media1.tenor.com/images/be14c627a8adc704b0582c23f582b255/tenor.gif?itemid=6142116" alt="Issou">
+      </div>
+      <div class="row centered">
+        <router-link class="button button-primary" :to="{name: 'home'}"><icon name="arrow-left"/>&nbsp;Retourner à l'accueil</router-link>
+      </div>
+    </main>
 </template>
 
 <script>
@@ -10,3 +16,5 @@ export default {
   name: "not-found"
 };
 </script>
+<style lang="scss" scoped>
+</style>
